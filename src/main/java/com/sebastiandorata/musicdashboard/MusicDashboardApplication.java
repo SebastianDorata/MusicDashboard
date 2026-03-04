0f12ceb27c2ecd255b0e4c47566803bf74b3108c
Sebastian Dorata <sebastiandorata@Sebastians-MacBook-Air.local>
2026-03-03 21:08:23 -0500

    Initial commit

---
+++ src/main/java/com/sebastiandorata/musicdashboard/MusicDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.sebastiandorata.musicdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicDashboardApplication.class, args);
+    }
+
+}
