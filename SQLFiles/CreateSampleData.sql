138c901ba480e21e09040c14dee51e0e58dc3102
sebastiandorata <sebastiandorata@gmail.com>
2026-04-06 13:43:47 -0400

    Randomized values

--- SQLFiles/CreateSampleData.sql
+++ SQLFiles/CreateSampleData.sql
@@ -1,4 +1,3 @@
-
 -- 2020 - JAN
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
 SELECT 8, 126, '2020-01-10 12:00:00', 3600, true FROM generate_series(1, 200);
@@ -9,43 +8,43 @@ SELECT 8, 127, '2020-02-10 12:00:00', 3600, true FROM generate_series(1, 220);
 
 -- 2020 - MAR
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 128, '2020-03-10 12:00:00', 3600, true FROM generate_series(1, 480);
+SELECT 8, floor(random() * 110 + 1)::int, '2020-03-10 12:00:00', 3600, true FROM generate_series(1, 480);
 
 -- 2020 - APR
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 129, '2020-04-10 12:00:00', 3600, true FROM generate_series(1, 650);
+SELECT 8, floor(random() * 110 + 1)::int, '2020-04-10 12:00:00', 3600, true FROM generate_series(1, 650);
 
 -- 2020 - MAY
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 130, '2020-05-10 12:00:00', 3600, true FROM generate_series(1, 530);
+SELECT 8, floor(random() * 110 + 1)::int, '2020-05-10 12:00:00', 3600, true FROM generate_series(1, 530);
 
 -- 2020 - JUN
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 131, '2020-06-10 12:00:00', 3600, true FROM generate_series(1, 550);
+SELECT 8, floor(random() * 110 + 1)::int, '2020-06-10 12:00:00', 3600, true FROM generate_series(1, 550);
 
 -- 2020 - JUL
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 132, '2020-07-10 12:00:00', 3600, true FROM generate_series(1, 520);
+SELECT 8, floor(random() * 110 + 1)::int, '2020-07-10 12:00:00', 3600, true FROM generate_series(1, 520);
 
 -- 2020 - AUG
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 133, '2020-08-10 12:00:00', 3600, true FROM generate_series(1, 500);
+SELECT 8, floor(random() * 110 + 1)::int, '2020-08-10 12:00:00', 3600, true FROM generate_series(1, 500);
 
 -- 2020 - SEP
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 134, '2020-09-10 12:00:00', 3600, true FROM generate_series(1, 550);
+SELECT 8, floor(random() * 110 + 1)::int, '2020-09-10 12:00:00', 3600, true FROM generate_series(1, 550);
 
 -- 2020 - OCT
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 135, '2020-10-10 12:00:00', 3600, true FROM generate_series(1, 400);
+SELECT 8, floor(random() * 110 + 1)::int, '2020-10-10 12:00:00', 3600, true FROM generate_series(1, 400);
 
 -- 2020 - NOV
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 136, '2020-11-10 12:00:00', 3600, true FROM generate_series(1, 450);
+SELECT 8, floor(random() * 110 + 1)::int, '2020-11-10 12:00:00', 3600, true FROM generate_series(1, 450);
 
 -- 2020 - DEC
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 137, '2020-12-10 12:00:00', 3600, true FROM generate_series(1, 460);
+SELECT 8, floor(random() * 110 + 1)::int, '2020-12-10 12:00:00', 3600, true FROM generate_series(1, 460);
 
 
 -- 2025 - JAN
@@ -58,43 +57,43 @@ SELECT 8, 127, '2025-02-10 12:00:00', 3600, true FROM generate_series(1, 160);
 
 -- 2025 - MAR
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 128, '2025-03-10 12:00:00', 3600, true FROM generate_series(1, 240);
+SELECT 8, floor(random() * 110 + 1)::int, '2025-03-10 12:00:00', 3600, true FROM generate_series(1, 240);
 
 -- 2025 - APR
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 129, '2025-04-10 12:00:00', 3600, true FROM generate_series(1, 200);
+SELECT 8, floor(random() * 110 + 1)::int, '2025-04-10 12:00:00', 3600, true FROM generate_series(1, 200);
 
 -- 2025 - MAY
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 130, '2025-05-10 12:00:00', 3600, true FROM generate_series(1, 180);
+SELECT 8, floor(random() * 110 + 1)::int, '2025-05-10 12:00:00', 3600, true FROM generate_series(1, 180);
 
 -- 2025 - JUN
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 131, '2025-06-10 12:00:00', 3600, true FROM generate_series(1, 120);
+SELECT 8, floor(random() * 110 + 1)::int, '2025-06-10 12:00:00', 3600, true FROM generate_series(1, 120);
 
 -- 2025 - JUL
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 132, '2025-07-10 12:00:00', 3600, true FROM generate_series(1, 480);
+SELECT 8, floor(random() * 110 + 1)::int, '2025-07-10 12:00:00', 3600, true FROM generate_series(1, 480);
 
 -- 2025 - AUG
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 133, '2025-08-10 12:00:00', 3600, true FROM generate_series(1, 320);
+SELECT 8, floor(random() * 110 + 1)::int, '2025-08-10 12:00:00', 3600, true FROM generate_series(1, 320);
 
 -- 2025 - SEP
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 134, '2025-09-10 12:00:00', 3600, true FROM generate_series(1, 250);
+SELECT 8, floor(random() * 110 + 1)::int, '2025-09-10 12:00:00', 3600, true FROM generate_series(1, 250);
 
 -- 2025 - OCT
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 135, '2025-10-10 12:00:00', 3600, true FROM generate_series(1, 500);
+SELECT 8, floor(random() * 110 + 1)::int, '2025-10-10 12:00:00', 3600, true FROM generate_series(1, 500);
 
 -- 2025 - NOV
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 136, '2025-11-10 12:00:00', 3600, true FROM generate_series(1, 350);
+SELECT 8, floor(random() * 110 + 1)::int, '2025-11-10 12:00:00', 3600, true FROM generate_series(1, 350);
 
 -- 2025 - DEC
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 137, '2025-12-10 12:00:00', 3600, true FROM generate_series(1, 300);
+SELECT 8, floor(random() * 110 + 1)::int, '2025-12-10 12:00:00', 3600, true FROM generate_series(1, 300);
 
 
 -- 2026 - JAN
@@ -104,12 +103,3 @@ SELECT 8, 126, '2026-01-10 12:00:00', 3600, true FROM generate_series(1, 250);
 -- 2026 - FEB
 INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
 SELECT 8, 127, '2026-02-10 12:00:00', 3600, true FROM generate_series(1, 320);
-
--- 2026 - MAR
-INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 128, '2026-03-10 12:00:00', 3600, true FROM generate_series(1, 180);
-
--- 2026 - APR
-INSERT INTO playback_history (user_id, song_id, played_at, duration_played_seconds, completed)
-SELECT 8, 129, '2026-04-10 12:00:00', 3600, true FROM generate_series(1, 300);
-
